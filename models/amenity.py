8a4b56f3f71d3235319c0807c7ad5a01fded6431
Bigizic <olalekanisaac75@gmail.com>
2023-08-21 13:02:43 +0000

    Add amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import models
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    if models.storage_temp == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place",
+                                       secondary="place_amenity",
+                                       back_populates="amenities")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor
+        """
+        super().__init__(*args, **kwargs)
