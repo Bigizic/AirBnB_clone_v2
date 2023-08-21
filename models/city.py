a8221e5d128c88e96b37983b9a6f13465a4bda82
Bigizic <olalekanisaac75@gmail.com>
2023-08-21 13:02:57 +0000

    Add city.py

--- models/city.py
+++ models/city.py
@@ -9,8 +9,8 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if models.storage_temp == 'db':
-        __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'),
                           nullable=False)
         name = Column(String(128), nullable=False)
