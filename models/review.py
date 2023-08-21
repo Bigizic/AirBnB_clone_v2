c110208408fa8edbe90a34ab254542d53518c6d7
Bigizic <olalekanisaac75@gmail.com>
2023-08-21 13:03:19 +0000

    Add review.py

--- models/review.py
+++ models/review.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import relationship
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
+    __tablename__ = 'reviews'
     if models.storage_temp == 'db':
-        __tablename__ = 'reviews'
         place_id = Column(String(60), ForeignKey('places.id'),
                           nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'),
