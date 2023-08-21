c99804229e755aef9ea3f6f027ab049fde045f09
Bigizic <olalekanisaac75@gmail.com>
2023-08-21 13:03:26 +0000

    Add state.py

--- models/state.py
+++ models/state.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
     if models.storage_temp == 'db':
-        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
                               #cascade="all, delete-orphan")
