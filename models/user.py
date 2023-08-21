b45401fdd7212749e91269b316e29232ce4ca233
Bigizic <olalekanisaac75@gmail.com>
2023-08-21 13:03:33 +0000

    Add user.py

--- models/user.py
+++ models/user.py
@@ -4,14 +4,13 @@ from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
-from models.base_model import BaseModel
 from models.base_model import Base
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     if models.storage_temp == 'db':
-        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
