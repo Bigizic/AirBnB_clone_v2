c0e27aa03b67120ca1a656fb475e4e0de87abaaa
Bigizic <olalekanisaac75@gmail.com>
2023-09-22 15:12:16 +0000

    Add web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,12 @@ tweaks
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
