12023e960ee35b77dd00a828ce4fbe5783252269
Bigizic <olalekanisaac75@gmail.com>
2023-09-24 12:32:39 +0000

    Add web_flask

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,9 +3,25 @@
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
+from models import storage
+import models
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
 app = Flask(__name__)
 
+
+@app.teardown_appcontext
+def close_session(exception):
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    s = sorted(storage.all(State).values(), key=lambda state: state.name)
+    a = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=s, amenities=a)
 
 
 if __name__ == '__main__':
